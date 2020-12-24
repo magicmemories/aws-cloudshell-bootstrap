a56f0a99d9f91170df4aa4b01b7caac211f3c233
VenkataMutyala <venkata.mutyala@outlook.com>
2020-12-24 00:59:22 -0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker cloudshell-user
+cd
+mkdir .ssh
+mv id_rsa .ssh/id_rsa
+eval "$(ssh-agent -s)"
+chmod 400 .ssh/id_rsa
+ssh-add -k ~/.ssh/id_rsa
+
